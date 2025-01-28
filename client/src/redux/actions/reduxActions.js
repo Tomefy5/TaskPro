cc5c880a0e249a6302e803bacbdefa4af40ed1fd
tomefy andry tsiresy <tomefyandrytsiresy@gmail.com>
2025-01-28 11:51:05 +0300

    refactor(redux): create actions folder and reduxActions.js for redux actions

---
+++ client/src/redux/actions/reduxActions.js
@@ -0,0 +1,72 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../services/api";
+
+// Action for fetching all tasks
+export const fetchTasks = createAsyncThunk(
+    "tasks/fetchTasks",
+    async (_, { rejectWithValue }) => {
+      try {
+        const response = await api.get("/fetch-tasks");
+        return response.data;
+      } catch (error) {
+        return rejectWithValue(error.response?.data || "Error on fetching tasks");
+      }
+    }
+  );
+  
+  // Action for creating new tasks
+  export const createTask = createAsyncThunk(
+    "tasks/createTask",
+    async (task, { rejectWithValue }) => {
+      try {
+        const response = await api.post("/create-task", task);
+        return response.data; // created task
+      } catch (error) {
+        return rejectWithValue(
+          error.response?.data || "Error on creating new task"
+        );
+      }
+    }
+  );
+  
+  // Action for updating tasks
+  export const updateTask = createAsyncThunk(
+    "tasks/updateTask",
+    async ({ taskId, update }, { rejectWithValue }) => {
+      try {
+        const response = await api.put(`/update-task/${taskId}`, update);
+        return response.data;
+      } catch (error) {
+        return rejectWithValue(error.response?.data || "Error on updating task");
+      }
+    }
+  );
+  
+  // Action for deleting tasks
+  export const deleteTask = createAsyncThunk(
+    "tasks/deleteTask",
+    async (taskId, { rejectWithValue }) => {
+      try {
+        await api.delete(`/delete-task/${taskId}`);
+      } catch (error) {
+        return rejectWithValue(error.response?.data || "Error on deleting task");
+      }
+    }
+  );
+  
+  // Action for changing task priority
+  export const changePriority = createAsyncThunk(
+    "tasks/changePriority",
+    async ({ taskId, newPriority }, { rejectWithValue }) => {
+      try {
+        const response = await api.put(
+          `/change-priority/${taskId}?priority=${newPriority}`
+        );
+        return response.data;
+      } catch (error) {
+        return rejectWithValue(
+          error.response?.data || "Error on changing task priority"
+        );
+      }
+    }
+  );
--- client/src/redux/slices/taskSlice.js
+++ client/src/redux/slices/taskSlice.js
@@ -1,75 +1,11 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import api from "../../services/api";
-
-// Action for fetching all tasks
-export const fetchTasks = createAsyncThunk(
-  "tasks/fetchTasks",
-  async (_, { rejectWithValue }) => {
-    try {
-      const response = await api.get("/fetch-tasks");
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.response?.data || "Error on fetching tasks");
-    }
-  }
-);
-
-// Action for creating new tasks
-export const createTask = createAsyncThunk(
-  "tasks/createTask",
-  async (task, { rejectWithValue }) => {
-    try {
-      const response = await api.post("/create-task", task);
-      return response.data; // created task
-    } catch (error) {
-      return rejectWithValue(
-        error.response?.data || "Error on creating new task"
-      );
-    }
-  }
-);
-
-// Action for updating tasks
-export const updateTask = createAsyncThunk(
-  "tasks/updateTask",
-  async ({ taskId, update }, { rejectWithValue }) => {
-    try {
-      const response = await api.put(`/update-task/${taskId}`, update);
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.response?.data || "Error on updating task");
-    }
-  }
-);
-
-// Action for deleting tasks
-export const deleteTask = createAsyncThunk(
-  "tasks/deleteTask",
-  async (taskId, { rejectWithValue }) => {
-    try {
-      await api.delete(`/delete-task/${taskId}`);
-    } catch (error) {
-      return rejectWithValue(error.response?.data || "Error on deleting task");
-    }
-  }
-);
-
-// Action for changing task priority
-export const changePriority = createAsyncThunk(
-  "tasks/changePriority",
-  async ({ taskId, newPriority }, { rejectWithValue }) => {
-    try {
-      const response = await api.put(
-        `/change-priority/${taskId}?priority=${newPriority}`
-      );
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(
-        error.response?.data || "Error on changing task priority"
-      );
-    }
-  }
-);
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  changePriority,
+  createTask,
+  deleteTask,
+  fetchTasks,
+  updateTask,
+} from "../actions/reduxActions";
 
 const taskSlice = createSlice({
   name: "tasks",
@@ -95,11 +31,11 @@ const taskSlice = createSlice({
         store.tasks = store.tasks.filter((task) => task._id !== action.payload);
       })
       .addCase(changePriority.fulfilled, (store, action) => {
-        store.tasks.map((task) => (
-          task.id === action.payload.taskId 
-          ? {...task, priority: action.payload.priority}
-          : task
-        ))
+        store.tasks.map((task) =>
+          task.id === action.payload.taskId
+            ? { ...task, priority: action.payload.priority }
+            : task
+        );
       });
   },
 });
